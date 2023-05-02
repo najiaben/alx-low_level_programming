b61047d9ec8b6f2580a8024aacfb5e9647de71bb
Najia Ben Abbou <najiabenabbou@gmail.com>
2023-05-02 09:59:42 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 int _putchar(char c);
 void rest_to_98(int *n);
-void swap_int?(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
