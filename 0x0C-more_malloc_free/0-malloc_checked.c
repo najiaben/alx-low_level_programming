1ce3e3543943ea0bf170335a23f226bdd3d71e5d
Najia Ben Abbou <najiabenabbou@gmail.com>
2023-05-21 17:00:46 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
+ * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
