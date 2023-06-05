6952476703428ac77fc5553f91db8347c1e0fb28
najiaben <najiabenabbou@gmail.com>
2023-06-05 16:21:32 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,6 +3,7 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: linked list of type listint_t to print
+ *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
